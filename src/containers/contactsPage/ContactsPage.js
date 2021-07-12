356ebd1cae623ca37a639c88a1339b194d5eccfc
kiranAch <ach@kiranAchs-Air.bbrouter>
2021-07-12 15:09:46 +0530

    add states and duplicate checker with useEffect

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ContactForm from '../../components/contactForm/ContactForm'
+import TitleList from '../../components/tileList/TileList'
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContacts }) => {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [duplicate, setDuplicate] = useState(false)
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    if (!duplicate) {
+      addContacts(name, phone, email)
+      setName('')
+      setPhone('')
+      setEmail('')
+    }
   }
-
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  useEffect(() => {
+    const duplicateName = () => {
+      const find = contacts.find((contact) => contact.name === name)
+      if (!find === undefined) {
+        return true
+      }
+      return false
+    }
+    if (duplicateName()) {
+      setDuplicate(true)
+    } else {
+      setDuplicate(false)
+    }
+  }, [name, duplicate, contacts])
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2>
+        <ContactForm
+          name
+          setName
+          phone
+          setPhone
+          email
+          setEmail
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TitleList title={contacts} />
       </section>
     </div>
   )
