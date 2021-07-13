ae9573452b07f748324871b513feee8d764aef78
kiranAch <ach@kiranAchs-Air.bbrouter>
2021-07-13 12:09:41 +0530

    add logic

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 export const ContactForm = ({
   name,
@@ -7,9 +7,36 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    ContactForm
-  );
-};
+    <form on onSubmit={handleSubmit}>
+      <input
+        type='text'
+        name='name'
+        value={name}
+        required
+        placeholder='Please enter a name'
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type='tel'
+        name='phone'
+        value={phone}
+        required
+        pattern='[[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}]'
+        placeholder='Please enter phone number'
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <input
+        type='email'
+        name='email'
+        value={email}
+        required
+        placeholder='Please enter an email'
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input type='submit' />
+    </form>
+  )
+}
